b55e35ccf3bf1d4d6643892a47c50b7a49511696
PabloD6003 <pablodavidgomezv@gmail.com>
2026-05-20 22:44:13 -0600

    feat: crear pagina base de donaciones

---
+++ src/pages/Donaciones/DonacionesPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './DonacionesPage.css'
+
+const DonacionesPage = () => {
+  return (
+    <div className="donaciones-container">
+      <h1>Gestión de Donaciones</h1>
+    </div>
+  )
+}
+
+export default DonacionesPage
