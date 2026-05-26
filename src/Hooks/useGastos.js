55ce1b3a0ccd5609ac172c753081a9d3b63d6ed4
Maria Paula Pérez Garcia <paulapgarciazzz@gmail.com>
2026-05-26 15:46:35 -0600

    componente Registro de gastos

--- src/Hooks/useGastos.js
+++ src/Hooks/useGastos.js
@@ -17,10 +17,10 @@ export default function useGastos(){
     const [error,setError] = useState(null)
     
     //Get gastos
-    const fecthGastos = useCallback(async () =>{
+    useEffect(() => {
+        const fetchGastos = async () =>{
         setLoading(true)
         try{
-            
             const response = await fetch(BASE_URL + '/latest', {headers})
             setGastos(response.data.record.gastos ?? [])
         }catch{
@@ -28,9 +28,34 @@ export default function useGastos(){
         }finally{
             setLoading(false)
         }
+    }
+        fetchGastos()
     },[])
-
-
-    return{}
+    
+    //create - agregar gasto
+    const agregarGasto = useCallback( async ({detalle, monto,descripcion, fecha_gasto}) =>{
+        setLoading(true) // Indicar que se está cargando
+        try{
+            const response = await fetch(BASE_URL + '/latest', {headers}) // Obtener los gastos actuales
+            const gastosActuales = response.data.record.gastos ?? [] 
+            // Crear un nuevo gasto con un ID único
+            const nuevoGasto = { 
+                id: Date.now(),
+                detalle,
+                monto: parseFloat(monto),
+                descripcion,
+                fecha_gasto, 
+            }
+            const gastosActualizados = [...gastosActuales, nuevoGasto]
+            await fetch.put(BASE_URL, {gastos: gastosActualizados}, {headers}) // Actualizar el estado local con los gastos actualizados
+            setGastos(gastosActualizados)
+        }catch{
+            setError('Error, no es posible agregar el gasto')
+        }finally{
+            setLoading(false) // Indicar que se ha terminado de cargar
+        }
+    },[])
+   
+    return{gastos,loading,error,agregarGasto}
     
 }
