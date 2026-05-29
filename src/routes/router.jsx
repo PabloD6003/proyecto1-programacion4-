e334556b233531577ce4f9825e590a60f26cc6ad
Maria Paula Pérez Garcia <paulapgarciazzz@gmail.com>
2026-05-29 00:28:07 -0600

    arreglando errores

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,5 +1,5 @@
 import { createRouter, createRoute, createRootRoute } from '@tanstack/react-router'
-import RegistroDeGastos from '../components/Registro de gastos'
+import RegistroDeGastos from '../components/RegistroDeGastos'
 import App from '../App'
 import BeneficiariosLayout from '../features/beneficiarios/BeneficiariosLayout'
 import BeneficiariosPage from '../features/beneficiarios/pages/BeneficiariosPage'
